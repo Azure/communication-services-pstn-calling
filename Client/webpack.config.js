2a90eb9a6e82c20fbf8d83e5f0fcb2f7584c6d22
Sander van Leeuwen <t-sanderv@microsoft.com>
2022-08-29 11:14:11 +0200

    Re-deleted styles

--- Client/webpack.config.js
+++ Client/webpack.config.js
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 const env = process.env;
 env.development = true;
@@ -12,54 +12,49 @@ const PORT = env.port || 9000;
 const isProd =
   env.development == null ||
   !env.development ||
-  env.development == 'false' ||
-  (env.production != null && (env.production == true || env.production == 'true'));
+  env.development == "false" ||
+  (env.production != null &&
+    (env.production == true || env.production == "true"));
 
 module.exports = {
-    ...(isProd ? {} : { devtool: 'eval-source-map' }),
-    mode: isProd ? 'production' : 'development',
-    entry: "./src/index.jsx",
-    output: {
-        path: path.join(__dirname, isProd ? 'dist/build': 'dist'),
-        filename: 'build.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: [/dist/, /node_modules/],
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader"
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
+  ...(isProd ? {} : { devtool: "eval-source-map" }),
+  mode: isProd ? "production" : "development",
+  entry: "./src/index.jsx",
+  output: {
+    path: path.join(__dirname, isProd ? "dist/build" : "dist"),
+    filename: "build.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/dist/, /node_modules/],
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './public/index.html',
-      filename: './index.html'
-    })
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
   ],
   devServer: {
     open: true,
@@ -67,13 +62,13 @@ module.exports = {
     port: PORT,
     proxy: [
       {
-        path: '/tokens/provisionUser',
-        target: 'http://[::1]:8080'
+        path: "/tokens/provisionUser",
+        target: "http://[::1]:8080",
       },
       {
-        path: '/connectionString',
-        target: 'http://[::1]:8080'
-      }
-    ]
-  }
+        path: "/connectionString",
+        target: "http://[::1]:8080",
+      },
+    ],
+  },
 };
