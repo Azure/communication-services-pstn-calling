eef009b708f03f0e8e1e6062e4022e9627d50d4a
Sander van Leeuwen <t-sanderv@microsoft.com>
2022-10-06 11:40:15 +0200

    Do not make appoptions mandatory

--- Server/Program.cs
+++ Server/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Configuration.AddJsonFile("appsettings.json", optional: false);
+builder.Configuration.AddJsonFile("appsettings.json", optional: true);
 builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddScoped<AppOptions, AppOptions>();
